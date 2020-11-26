e33f987def9a229fed12f71e697e5ff0f4df7886
dreamer2q <dreamer22qq@gmail.com>
2020-11-27 00:33:45 +0800

    update

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
-var baseUrl = "http://localhost:8080/api";
+var baseUrl = "https://api.dreamer2q.wang/api";
 
 function generateArticle(article) {
     let href = `article.html?slug=${article.slug}`;
@@ -37,8 +37,50 @@ function generateArticle(article) {
 </article>`;
 }
 
-function checkResp(resp) {
+function generateDetail(article) {
+    let markdownHtml = marked(article.body);
+    return `<article class="post">
+    <div class="post-header">
+        <h1 class="post-title">
+            ${article.title}
+        </h1>
+        <div class="post-meta">
+            <span class="post-time">
+                <span class="post-meta-item-icon">
+                    <i class="fa fa-calendar-o"></i>
+                </span>
+                <span class="post-meta-item-text">发布于</span>
+                <span class="post-meta-item-time">${article.first_published_at}</span>
+            </span>
+            <span class="post-category">
+                <span class="post-meta-divider">|</span>
+                <span class="post-meta-item-icon"><i class="fa fa-folder-o"></i></span>
+                <span class="post-meta-item-text">分类于</span>
+                <span><a href="#"><span>Report</span></a></span>
+            </span>
+        </div>
+    </div>
+    <div class="post-body" id="markdown-body">
+            ${article.body_html}
+    </div>
+    <div class="post-footer">
+    </div>
+</article>`;
+}
+
+function parseResp(resp) {
     if (resp.code != 200) {
         throw `error ${resp.code} : ${resp.message}`;
     }
+    return resp.data;
+}
+
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
 }
